6d80a213208970bd2a44ec894848a2ceb6900dbf
Vladan Petrovic <vpetrovic@edeja.com>
2018-09-22 21:59:55 +0200

    Init

---
+++ Console/CSharpSpecs/CSharpSpecs/MainEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpSpecs
+{
+    public class MainEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
